269d85ff41a229de821c38d7b5157a73f9854358
OtavioLira <otavioliraneves@gmail.com>
2024-09-12 10:39:18 -0300

    feat - raspagem de informações das vagas

--- main.py
+++ main.py
@@ -20,18 +20,44 @@ try:
 
     time.sleep(5)
 
-    # Procurando input de pesquisa
+    # Pesquisando vaga
     input_vaga = driver.find_element(By.NAME, value="q")
     input_vaga.send_keys("Estágio")
     input_vaga.submit()
 
-    # pesquisando vaga
-    submit_button = driver.find_element(By.NAME, value="submit")
-    submit_button.submit()
+    # Procurando lista de vagas
+    lista_vagas = driver.find_elements(By.XPATH, "//ul[contains(@class, 'gtm-class search-result-custom_jobList')]/li")
+
+    # Verificando se encontrou vagas
+    if lista_vagas:
+        print(f"Encontrado {len(lista_vagas)} vagas.")
+        for vaga in lista_vagas:
+            print("-"*10)
+            print(vaga.text.split("\n"))  # Exibe o texto de cada vaga
+            vaga = vaga.text.split("\n")
+
+            # Raspagem de informações
+            nome_vaga = vaga[0]
+            empresa_vaga = vaga[1]
+            descricao_vaga = vaga[2]
+            salario_vaga = vaga[3]
+            localizacao_vaga = vaga[4]
+
+            print(f""" Sobre a vaga
+            nome da vaga: {nome_vaga}
+            empresa da vaga: {empresa_vaga}
+            descricao da vaga: {descricao_vaga}
+            salario da vaga: {salario_vaga}
+            localização da vaga: {localizacao_vaga}
+            """)
+
+            print("-"*10)
+    else:
+        print("Nenhuma vaga encontrada.")
 
     time.sleep(5)
 except Exception as ex:
     print(f"Erro, {ex}")
 finally:
     print("Finalizando processo")
-    #driver.quit()
+    driver.quit()
