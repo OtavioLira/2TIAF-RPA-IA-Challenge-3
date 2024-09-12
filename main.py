e0ecc06ed68e9bc91fdbafb1a112b8c0347e8b55
OtavioLira <otavioliraneves@gmail.com>
2024-09-12 11:40:41 -0300

    feat - dataset pandas + template readme

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service as FirefoxService
+#from selenium.webdriver.firefox.service import Service
+#from webdriver_manager.firefox import GeckoDriverManager
+#from selenium.webdriver.firefox.options import Options
+#from selenium.webdriver.firefox.service import Service as FirefoxService
 import time
+import pandas as pd
 
-#from selenium.webdriver.chrome.service import Service
-#from webdriver_manager.chrome import ChromeDriverManager
-#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
  
 driver = webdriver.Chrome()
 url = "https://www.catho.com.br/"
-Continue = False
+
+dataset_vagas = {
+    "nome": [],
+    "empresa": [],
+    "descricao": [],
+    "salario": [],
+    "vaga": []
+}
+dataset_vagas = pd.DataFrame(dataset_vagas)
 
 try:
     # Abrindo o navegador
@@ -28,12 +37,12 @@ try:
     # Procurando lista de vagas
     lista_vagas = driver.find_elements(By.XPATH, "//ul[contains(@class, 'gtm-class search-result-custom_jobList')]/li")
 
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
     # Verificando se encontrou vagas
     if lista_vagas:
         print(f"Encontrado {len(lista_vagas)} vagas.")
         for vaga in lista_vagas:
-            print("-"*10)
-            print(vaga.text.split("\n"))  # Exibe o texto de cada vaga
             vaga = vaga.text.split("\n")
 
             # Raspagem de informações
@@ -43,15 +52,15 @@ try:
             salario_vaga = vaga[3]
             localizacao_vaga = vaga[4]
 
-            print(f""" Sobre a vaga
-            nome da vaga: {nome_vaga}
-            empresa da vaga: {empresa_vaga}
-            descricao da vaga: {descricao_vaga}
-            salario da vaga: {salario_vaga}
-            localização da vaga: {localizacao_vaga}
-            """)
+            nova_linha = pd.DataFrame([{
+                "nome": nome_vaga,
+                "empresa": empresa_vaga,
+                "descricao": descricao_vaga,
+                "salario": salario_vaga,
+                "vaga": localizacao_vaga
+            }])
 
-            print("-"*10)
+            dataset_vagas = pd.concat([dataset_vagas, nova_linha], ignore_index=True)
     else:
         print("Nenhuma vaga encontrada.")
 
@@ -60,4 +69,5 @@ except Exception as ex:
     print(f"Erro, {ex}")
 finally:
     print("Finalizando processo")
+    print(dataset_vagas)
     driver.quit()
