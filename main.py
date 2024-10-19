f5418c727168f5f75f1345556868c722a4713e76
OtavioLira <otavioliraneves@gmail.com>
2024-10-19 00:54:51 -0300

    feat - Melhorando código principal - funcionamento por json

--- main.py
+++ main.py
@@ -1,129 +1,94 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-#from selenium.webdriver.firefox.service import Service
-#from webdriver_manager.firefox import GeckoDriverManager
-#from selenium.webdriver.firefox.options import Options
-#from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-
 import time
 import pandas as pd
+import re
+import json
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 
+def remove_special_chars(text):
+    return re.sub(r'[^\w\s]', '', text)
 
-driver = webdriver.Chrome()
-url_catho = "https://www.catho.com.br/"
-url_gupy = "https://portal.gupy.io/"
+def initial_config():
+    webpages = []
+    for fileConfig in os.listdir("config"):
+        if fileConfig.endswith(".json"):
+            full_path = os.path.join("config", fileConfig)
+            with open(full_path, "r") as f:
+                data = json.load(f)
+                webpages.append(data)
+    return webpages
 
-dataset_vagas = {
-    "nome": [],
-    "empresa": [],
-    "descricao": [],
-    "salario": [],
-    "vaga": []
-}
+def process():
+    dataset_vagas = {
+        "nome": [],
+        "empresa": [],
+        "descricao": [],
+        "salario": [],
+        "localizacao": [],
+        "link vaga": []
+    }
 
-dataset_vagas = pd.DataFrame(dataset_vagas)
+    dataset_vagas = pd.DataFrame(dataset_vagas)
 
-cookies_aceito = False
+    for webpage in initial_config():
+        # Abrir página do site
+        driver = webdriver.Chrome()
+        driver.get(webpage["url"])
 
+        #Pesquisando vaga
+        input_vaga = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, webpage["pesquisa_vaga"]["xpath"]))
+        )
+
+        input_vaga.send_keys("Estágio")
+        input_vaga.submit()
+
+        time.sleep(5)
+        
+        # Descer página
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        #Pesquisar vagas
+        lista_vagas = driver.find_elements(By.XPATH, webpage["lista_vagas"]["xpath"])
+
+         # Verificando se sencontrou vagass
+        if lista_vagas:
+            print(f"Encontrado {len(lista_vagas)} vagas na {webpage['nome']}.")
+            link_vagas = []
+            for vaga in lista_vagas:
+                link_vagas.append(vaga.find_element(By.XPATH, webpage["lista_vagas"]["xpath_link"]).get_property("href"))
+            for link in link_vagas:
+                # Abrindo o navegadors
+                driver.get(link)
+
+                # Raspagem de informaçõesss
+                nome_vaga = driver.find_element(By.XPATH, webpage["elementos_vaga"]["nome"]["xpath"]).text
+                empresa_vaga = driver.find_element(By.XPATH, webpage["elementos_vaga"]["empresa"]["xpath"]).text 
+                descricao_vaga = driver.find_element(By.XPATH, webpage["elementos_vaga"]["descricao"]["xpath"]).text
+                salario_vaga = driver.find_element(By.XPATH, webpage["elementos_vaga"]["salario"]["xpath"]).text
+                localizacao_vaga = driver.find_element(By.XPATH, webpage["elementos_vaga"]["localizacao"]["xpath"]).text
+
+                nova_linha = pd.DataFrame([{
+                    "nome": nome_vaga,
+                    "empresa": empresa_vaga,
+                    "descricao": descricao_vaga, 
+                    "salario": salario_vaga,
+                    "localizacao": localizacao_vaga,
+                    "link vaga": link
+                }])
+
+                dataset_vagas = pd.concat([dataset_vagas,nova_linha], ignore_index=True)
+    driver.quit()
+
+    # Aplicando a função para remover caracteres especiais
+    dataset_vagas['descricao'] = dataset_vagas['descricao'].apply(remove_special_chars)
+    dataset_vagas.to_excel("Vagas-Catho.xlsx")
 try:
-    # Abrindo o navegador
-    driver.get(url_catho)
-
-    # Pesquisando vaga
-    input_vaga = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "q"))
-    )
-
-    input_vaga.send_keys("Estágio")
-    input_vaga.submit()
-
-    # Procurando lista de vsagas
-    time.sleep(5)
-    lista_vagas = driver.find_elements(By.XPATH, "//ul[contains(@class, 'gtm-class search-result-custom_jobList')]/li")
-
-    # Verificando se sencontrou vagass
-    if lista_vagas:
-        print(f"Encontrado {len(lista_vagas)} vagas na Catho.")
-        link_vagas = []
-        for vaga in lista_vagas:
-            link_vagas.append(vaga.find_element(By.XPATH,".//a").get_property("href"))
-        for link in link_vagas:
-            # Abrindo o navegadors
-            driver.get(link)
-
-            # Raspagem de informaçõesss
-            nome_vaga = driver.find_element(By.XPATH,"//header[contains(@class, 'Header-module')]//h1").text
-            empresa_vaga = driver.find_element(By.XPATH,".//div[contains(@class, 'info-item')]").text
-            descricao_vaga = driver.find_element(By.XPATH, ".//div[contains(@class, 'job-description')]")
-            salario_vaga = driver.find_element(By.XPATH,".//article[contains(@id, 'job')]//ul//li").text
-            localizacao_vaga = driver.find_element(By.XPATH,".//div[contains(@class, 'cidades')]//button/a").text
-
-            nova_linha = pd.DataFrame([{
-                "nome": nome_vaga,
-                "empresa": empresa_vaga,
-                "descricao": descricao_vaga, 
-                "salario": salario_vaga,
-                "vaga": localizacao_vaga
-            }])
-
-            dataset_vagas = pd.concat([dataset_vagas,nova_linha], ignore_index=True)
-        # codigo para continuar para outras paginas (não consegui fazer)
+    process()
 except Exception as err:
     print(f"Erro, {err}")
-
-try:
-    # Abrindo o navegador
-    driver.get(url_gupy)
-
-    # Pesquisando vaga
-    input_vaga = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "searchTerm"))
-    )
-
-    input_vaga.send_keys("Programador Junior")
-    input_vaga.submit()
-
-    # Procurando lista de vsagas
-    time.sleep(5)
-    lista_vagas = driver.find_elements(By.XPATH, "//ul[contains(@class,'sc-a01de6b')]/li")
-
-    # Verificando se sencontrou vagass
-    if lista_vagas:
-        print(f"Encontrado {len(lista_vagas)} vagas na GUPY.")
-        link_vagas = []
-        empresa_vagas = []
-        for vaga in lista_vagas:
-            link_vagas.append(vaga.find_element(By.XPATH,".//a[contains(@class, 'sc-4d881605-1 IKqnq')]").get_property("href"))
-            empresa_vagas.append(driver.find_element(By.XPATH, "//p[contains(@class, 'sc-bBXxYQ eJcDNr sc-4d881605-5 bpsGtj')]").text)
-        for i in range(len(link_vagas)):
-            # Ir para pagina da vaga
-            driver.get(link_vagas[i])
-            time.sleep(5)
-
-            # Raspagem de informações
-            nome_vaga = driver.find_element(By.XPATH, "//*[@id='h1']").text
-            descricao_vaga = driver.find_element(By.XPATH, "//*[@id='main']/div/section[1]//div").text
-            salario_vaga = '-'
-            localizacao_vaga = driver.find_element(By.XPATH, "//*[@id='main']/div/div[1]/div/div[2]/div[4]/p[1]/span[2]").text
-            
-
-            nova_linha = pd.DataFrame([{
-                "nome": nome_vaga,
-                "empresa": empresa_vagas[i],
-                "descricao": descricao_vaga,
-                "salario": salario_vaga,
-                "vaga": localizacao_vaga
-            }])
-
-            dataset_vagas = pd.concat([dataset_vagas,nova_linha], ignore_index=True)
-except Exception as err:
-    print(f"Erro, {err}")
-
-dataset_vagas.to_excel("Vagas-Catho.xlsx")
-driver.quit()
+finally:
+    print("------Fim do processo-----")
